a7af25f9583aa7fe36aae85144b15e71f29001fc
drtvan <dremu@tuta.io>
2024-11-10 18:34:58 +0100

    start

---
+++ README.md
@@ -0,0 +1,4 @@
+# ToDoTrackerCLI
+
+In-progress Roadmap.sh Task Tracker project:
+https://roadmap.sh/projects/task-tracker
--- main.cpp
+++ main.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
+#include <nlohmann/json.hpp>
+#include <fstream>
 
+using json = nlohmann::json;
 using namespace std;
 
-int main(int argc, char* argv[]) {
+#if defined(_WIN32) || defined(_WIN64)
+    #define PLATFORM_NAME "cancer"
+#elif defined(__linux__) || defined(__APPLE__)
+    #define PLATFORM_NAME "unix"
+#endif
 
+bool CheckFile() {
+    if constexpr (PLATFORM_NAME == "cancer") {
+
+    } else if constexpr (PLATFORM_NAME == "unix") {
+
+    }
+}
+
+void List() {
+
+}
+
+void Add() {
+
+}
+
+void Delete() {
+
+}
+
+int main(int argc, char* argv[]) {
+    CheckFile();
     return 0;
 }
